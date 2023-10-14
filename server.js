013fac4c1d52a030e843373d904c301f0292e34d
serenov <73263527+serenov@users.noreply.github.com>
2023-10-14 17:37:37 +0530

    updated environmnet variables

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
 
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
@@ -18,5 +18,5 @@ app.use(express.json());
 app.use('/api', require('./routes/gateway')); 
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http:://127.0.0.1:${port} URI`);
 });
